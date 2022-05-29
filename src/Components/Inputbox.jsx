645e2c777bf9f9ff5a8d9e23853fb5e6b1514789
sachinrawagent <sa147831@gmail.com>
2022-05-29 20:31:43 +0530

    setting state for button shoot and ballon

--- src/Components/Inputbox.jsx
+++ src/Components/Inputbox.jsx
@@ -9,6 +9,12 @@ function Inputbox() {
         const {className,value}=e.target;
         setData({...data,[className]:value,})
     }
+    const [order, setOrder] = useState([]);
+    const [c1, setC1] = useState({});
+    const [c2, setC2] = useState({});
+    const [c3, setC3] = useState({});
+    const [c4, setC4] = useState({});
+    const [c5, setC5] = useState({});
     const submit=(e)=>{
         e.preventDefault();
         // if(data ==="1"|| data ==="2"|| data==="3"|| data==="4"|| data==="5"){
